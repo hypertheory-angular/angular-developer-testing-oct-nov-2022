0dcfa75057a0582487f051f3f87691248cd63b99
Jeffry Gonzalez <Jeff@hypertheory.com>
2022-10-31 15:23:46 -0400

    sort of fixed

--- libs/learning/src/lib/utils.spec.ts
+++ libs/learning/src/lib/utils.spec.ts
@@ -1,4 +1,4 @@
-import { sendReminder } from './utils';
+import { giveRaise, Retiree, sendReminder, Worker } from './utils';
 describe('the utilities library', () => {
   describe('sending reminders', () => {
     it('does stuff', () => {
@@ -19,5 +19,29 @@ describe('the utilities library', () => {
       // Duck Typing (Structural Typing)
       sendReminder(person);
     });
+  });
+});
+
+describe('Giving Raises', () => {
+  it('Giving an employee a raise', () => {
+    const ed: Worker = {
+      salary: 100_000,
+      name: 'Ed Smith',
+    };
+
+    giveRaise(ed, 100);
+
+    expect(ed.salary).toBe(100_100);
+  });
+
+  it('giving a retiree a raise', () => {
+    const jill: Worker = {
+      name: 'Jill',
+      pension: 80_000,
+    };
+
+    giveRaise(jill, 100);
+
+    expect(jill.pension).toBe(80_100);
   });
 });
--- libs/learning/src/lib/utils.ts
+++ libs/learning/src/lib/utils.ts
@@ -9,3 +9,34 @@ export type ReminderRecipient = {
   email: string;
   sendEmail: () => void;
 };
+
+type Person = {
+  name: string;
+};
+
+export type Employee = Person & {
+  salary: number;
+  pension?: never;
+};
+
+export type Retiree = Person & {
+  pension: number;
+  salary?: never;
+};
+
+export type Worker = Employee | Retiree;
+
+export function giveRaise(worker: Worker, amount: number) {
+  if (worker.salary) {
+    return {
+      ...worker,
+      salary: worker.salary + amount,
+    } as Employee;
+  } else if (worker.pension) {
+    return {
+      ...worker,
+      pension: worker.pension + amount,
+    } as Retiree;
+  }
+  return undefined;
+}
